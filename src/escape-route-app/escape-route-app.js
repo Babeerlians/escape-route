345f676c051e6488715caa24fb7528556e26cd1b
Juan Antonio Parés Ponce <juan.pares@babel-si.local>
2018-05-14 12:15:31 +0200

    card style

--- src/escape-route-app/escape-route-app.js
+++ src/escape-route-app/escape-route-app.js
@@ -13,14 +13,27 @@ class EscapeRouteApp extends PolymerElement {
           display: block;
         }
       
-        .center {
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        .card {
+          margin: 24px;
+          padding: 16px;
+          color: #757575;
+          border-radius: 5px;
+          background-color: #fff;
+          box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+          text-align: center;
         }
       </style>
-      <h1 class="center">Escape Route</h1>
-      <login-module class="center"></login-module>
+      
+      <app-drawer-layout fullbleed>
+        <app-header-layout>
+          <iron-pages role="main">
+            <div class="card">
+              <h1>Escape Route</h1>
+              <login-module></login-module>
+            </div>
+          </iron-pages>
+        </app-header-layout>
+      </app-drawer-layout>
     `;
   }
 
