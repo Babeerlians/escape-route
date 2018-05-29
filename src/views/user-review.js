33e97efba09756ab13e74efcea4e50516da7d12f
Luis Romero <versatil.lr.92@gmail.com>
2018-05-29 11:21:35 +0200

    Add valoration avg

--- src/views/user-review.js
+++ src/views/user-review.js
@@ -14,22 +14,25 @@ import '../styles/shared-styles.js';
 
 class UserReview extends PolymerElement {
     static get template() {
-        return html `
+        return html`
             <style include="shared-styles">
-                .valorations {
+                .valoration {
                     display: flex;
                 }
+            
                 .review {
                     display: flex;
                 }
+            
                 .date {
                     display: flex;
-                    justify-content: space-around;
+                    justify-content: space-evenly;
                     align-items: center;
                 }
+            
                 @media (min-width: 1001px) {
                     .review {
-                        justify-content: space-around;
+                        justify-content: space-evenly;
                     }
                     .search {
                         display: block;
@@ -37,58 +40,65 @@ class UserReview extends PolymerElement {
                     .notes {
                         width: 100%;
                     }
-                    .valorations {
+                    .valoration {
                         width: 30%;
                         flex-direction: column;
                         margin-left: 5%;
                     }
                 }
+            
                 @media (max-width: 640px) {
                     .review {
                         flex-direction: column;
                     }
                 }
+            
                 @media (max-width: 800px) {
-                    .valorations {
+                    .valoration {
                         flex-direction: column;
                         display: inline-grid;
                     }
                 }
+            
                 @media (max-width: 1000px) {
                     .review {
                         flex-direction: column;
                         margin: 5px;
                     }
-                    .valorations {
-                        justify-content: space-around;
+                    .valoration {
+                        justify-content: space-evenly;
                     }
                 }
+            
                 paper-textarea {
                     background-color: var(--app-secondary-color)
                 }
-                @media (max-width: 640px){
+            
+                @media (max-width: 640px) {
                     .review {
                         flex-direction: column;
                     }
                 }
+            
                 .buttons {
                     display: flex;
                     justify-content: space-evenly;
                 }
+            
                 .submit {
                     margin-top: 20px;
                 }
+            
                 .submit[disabled] {
                     opacity: 0.5;
                 }
+            
                 paper-toggle-button {
-                    --paper-toggle-button-checked-bar-color:  var(--app-primary-color);
-                    --paper-toggle-button-checked-button-color:  var(--app-primary-color);
-                    --paper-toggle-button-unchecked-bar-color:  var(--app-tertiary-color);
-                    --paper-toggle-button-unchecked-button-color:  var(--app-secondary-color);
+                    --paper-toggle-button-checked-bar-color: var(--app-primary-color);
+                    --paper-toggle-button-checked-button-color: var(--app-primary-color);
+                    --paper-toggle-button-unchecked-bar-color: var(--app-tertiary-color);
+                    --paper-toggle-button-unchecked-button-color: var(--app-secondary-color);
                 }
-
-                
             </style>
             <div class="card">
                 <div class="search">
@@ -97,10 +107,10 @@ class UserReview extends PolymerElement {
                 <div class="review">
                     <div class="notes">
                         <h2>Note</h2>
-                        <paper-textarea id="tanotes" placeholder="¿Te ha gustado el juego?¿Lo recomendarías? ¿Qué es lo mejor que tiene y qué destacarías? ¿Tiene algo que no te haya gustado?¿Qué cambios o mejoras sugerirías?" 
-                        rows=4 maxlength=300 value="{{note}}"></paper-textarea>
+                        <paper-textarea id="tanotes" placeholder="¿Te ha gustado el juego?¿Lo recomendarías? ¿Qué es lo mejor que tiene y qué destacarías? ¿Tiene algo que no te haya gustado?¿Qué cambios o mejoras sugerirías?"
+                            rows=4 maxlength=300 value="{{note}}"></paper-textarea>
                     </div>
-                    <div class="valorations">
+                    <div class="valoration">
                         <icon-toggle id="general" total=5 icon="star" value={{general}} title="General"></icon-toggle>
                         <icon-toggle id="difficulty" total=5 icon="lock" value={{difficulty}} title="Difficulty"></icon-toggle>
                         <icon-toggle id="ambience" total=5 icon="home" value={{ambience}} title="Ambience"></icon-toggle>
@@ -113,15 +123,15 @@ class UserReview extends PolymerElement {
                     <h2>Fecha</h2>
                     <div class="date">
                         <vaadin-date-picker-light id="vdate" value="{{date}}">
-                                Fecha:
-                                <iron-icon icon="event"></iron-icon>
-                                <iron-input>
-                                    <input size="10" disabled>
-                                </iron-input>
+                            Fecha:
+                            <iron-icon icon="event"></iron-icon>
+                            <iron-input>
+                                <input size="10" disabled>
+                            </iron-input>
                         </vaadin-date-picker-light>
                         <paper-toggle-button on-change="toggleDuration" checked="{{completed}}">Completed</paper-toggle-button>
                         <time-picker id="duration" minutes="{{minutes}}" seconds="{{seconds}}" class="not-visible"></time-picker>
-                    </div>                 
+                    </div>
                 </div>
                 <div class="buttons">
                     <paper-button raised class="submit" on-click="_discardReview">Discard</paper-button>
@@ -188,10 +198,10 @@ class UserReview extends PolymerElement {
 
     validate(general, difficulty, ambience, note, idescape, completed, date, minutes, seconds) {
         if (general > 0 && difficulty > 0 && ambience > 0 && note.length > 0 && idescape.length > 0 && date.length > 0) {
-            if(completed === false || (completed === true && (minutes > 0 || seconds > 0))){
+            if (completed === false || (completed === true && (minutes > 0 || seconds > 0))) {
                 this.$.btnSave.removeAttribute('disabled');
             }
-            else{
+            else {
                 this.$.btnSave.setAttribute('disabled', true);
             }
         } else {
@@ -206,11 +216,11 @@ class UserReview extends PolymerElement {
     _discardReview() {
         this.dispatchEvent(new CustomEvent('discarded'));
     }
-    
+
     _saveReview() {
         var review = {
             [this.idescape]: {
-                valorations: {
+                valoration: {
                     general: this.general,
                     difficulty: this.difficulty,
                     ambience: this.ambience
@@ -229,11 +239,34 @@ class UserReview extends PolymerElement {
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
                 firebase.database().ref('users/' + user.uid + '/reviews').update(review).then(() => {
-                        this.dispatchEvent(new CustomEvent('saved'));
-                    })
-                    .catch((error) => {
-                        console.log('Synchronization failed');
+                    firebase.database().ref('users').once('value', snapshot => {
+                        let valorationAcc = {
+                            valoration: {
+                                ambience: 0,
+                                difficulty: 0,
+                                general: 0
+                            }
+                        };
+                        let reviews = Object.values(snapshot.val()).filter(user =>
+                            user.reviews && user.reviews.hasOwnProperty(this.idescape)
+                        ).map(item => item.reviews[this.idescape]);
+
+                        let valorationAverage = reviews.reduce((acc, item) => {
+                            acc.valoration.ambience += item.valoration.ambience / reviews.length;
+                            acc.valoration.difficulty += item.valoration.difficulty / reviews.length;
+                            acc.valoration.general += item.valoration.general / reviews.length;
+                            return acc;
+                        }, valorationAcc);
+
+                        firebase.database().ref('games/'+this.idescape).update(valorationAverage).then(() => {
+                            this.dispatchEvent(new CustomEvent('saved'));
+                        }).catch((error) => {
+                            console.log('Synchronization failed');
+                        });
                     });
+                }).catch((error) => {
+                    console.log('Synchronization failed');
+                });
             }
         });
     }
